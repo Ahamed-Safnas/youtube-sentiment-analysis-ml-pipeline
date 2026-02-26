78ce7fe5fdff95d55016cc8edddba9fde89a157c
safnasNED25 <safnas2042ahamed@gmail.com>
2026-02-26 16:00:04 +0530

    bug fixed

--- src/model/model_evaluation.py
+++ src/model/model_evaluation.py
@@ -127,8 +127,8 @@ def save_model_info(run_id: str, model_path: str, file_path: str) -> None:
 
 
 def main():
-    mlflow.set_tracking_uri("http://ec2-54-167-108-249.compute-1.amazonaws.com:5000/")
-
+    mlflow.set_tracking_uri("http://ec2-44-200-181-172.compute-1.amazonaws.com:5000/")
+    # http://ec2-44-200-181-172.compute-1.amazonaws.com:5000/
     mlflow.set_experiment('dvc-pipeline-runs')
     
     with mlflow.start_run() as run:
